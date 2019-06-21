742f5d56af1d4691a38fbe56f49c70afc3e87012
mkeren22 <51344504+mkeren22@users.noreply.github.com>
2019-06-21 13:15:08 -0400

    Update p5-sketch1.js

--- p5-sketch1.js
+++ p5-sketch1.js
@@ -3,22 +3,24 @@
 
 function sketch(parent) { // we pass the sketch data from the parent
     return function( p ) {
+        
+      // create new osc
+      var osc = new p5.Oscillator();
+      
       p.setup = function() {
         let canvas = p.createCanvas(400, 400);
         canvas.parent(parent.$el); // links child to parent
-        // create new osc
-        let osc = new p.p5.Oscillator();
-        p.osc.setType('sine');
-        p.osc.freq(400);
-        p.osc.amp(0);
-        p.osc.start();
+        osc.setType('sine');
+        osc.freq(400);
+        osc.amp(0);
+        osc.start();
   
       };
   
       p.draw = function() {
         p.background(100); // arbitrary color
-        p.osc.amp(parent.data.amp);
-        p.osc.freq(parent.data.baseFreq);
+        osc.amp(parent.data.amp);
+        osc.freq(parent.data.baseFreq);
       };
     };
   }
